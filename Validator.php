3366fdb58a1a0a03ca0f6e0ae0c134610d69ac7c
shu <202504ap01_shu@x-ide.sakura.ne.jp>
2025-07-10 15:02:36 +0900

    名前の前後に空白のチェックコードを修正しました。
    この前に、名字と名前の間に空白が許容することをしました。

--- Validator.php
+++ Validator.php
@@ -14,7 +14,7 @@ class Validator
             $this->error_message['name'] = '名前が入力されていません';
         } elseif (preg_match('/^[\s　]+$/u', $data['name'])) {
             $this->error_message['name'] = '名前に空白だけを入力することはできません';
-        } elseif (preg_match('/^\s|　|\s$|　$/u', $data['name'])) {
+        } elseif ($data['name'] !== trim($data['name'], " 　")) {
             $this->error_message['name'] = '名前の前後に空白を入れないでください';
         } elseif (mb_strlen($data['name']) > 20) {
             $this->error_message['name'] = '名前は20文字以内で入力してください';
